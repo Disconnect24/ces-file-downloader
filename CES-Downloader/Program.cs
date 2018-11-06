eefb8a0b2b170db5d80ee5d345177e27a59f20a8
Marko <32610623+Apfel@users.noreply.github.com>
2018-11-06 18:06:21 +0100

    Epic

---
+++ CES-Downloader/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+
+namespace CES_Downloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length == 0) Console.WriteLine("No args.");
+            else if (args[0] == "all")
+            {
+                if (!Directory.Exists("files")) Directory.CreateDirectory("files");
+                else
+                {
+                    DirectoryInfo tempdir = new DirectoryInfo("files");
+                    foreach (FileInfo file in tempdir.GetFiles()) file.Delete();
+                }
+                int count = 0;
+                while (count <= 255)
+                {
+                    StreamWriter stream = File.CreateText($"files\\{count}.ces");
+                    stream.Write(GetData(count)); stream.Close(); count++;
+                }
+            }
+            else if (int.TryParse(args[0], out int id))
+            {
+                if (File.Exists($"{id}.ces")) File.Delete($"{id}.ces");
+                StreamWriter stream = File.CreateText($"{id}.ces");
+                stream.Write(GetData(id)); stream.Close();
+            }
+            else Console.WriteLine("Invalid args.");
+        }
+
+        static byte[] GetData(int id) => new HttpClient().GetByteArrayAsync($"http://miicontest.wapp.wii.com/first/{id}.ces").GetAwaiter().GetResult();
+    }
+}
---
+++ CES-Downloader/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CES-Downloader")]
+[assembly: AssemblyDescription("Download the CES files used by the Check Mii Out-Channel with ease.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CES-Downloader")]
+[assembly: AssemblyCopyright("Copyright © Apfel 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("dcfce228-6a4a-4f9d-95b3-814fb25e0d0c")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
